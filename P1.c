ba30db8de5f29f2e7987790d81c4450ce4c0d071
Lucas Guaragna Guedes <37838183+lucasguaragna@users.noreply.github.com>
2018-06-20 14:42:07 -0300

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,14 +1,14 @@
-//Nome: Lucas Guaragna Guedes	
-//Matricula: 11721EBI005
+//Lucas Guaragna Guedes
+//11721EBI005
 
 #include <stdio.h> 
-
 int main()
+
 {
 	char num[256];
 	int x = 0, r = 0;
-	printf_s("\nDigite o número binário: ");
-	scanf_s("%s", &num);
+	printf("\nDigite o numero binario: ");
+	scanf("%s", &num);
 	for(int i = 0; num [i] != '\0'; i++)
 	{
 		if(num[i] == '0' && x == 0)r = 0;
@@ -20,11 +20,7 @@ int main()
 	x = r;
 	r = 0;
 	}
-	
-	if(x == 0)
-	{
-	printf("O número binário %s é múltiplo de 3", num);
-	else printf("O número binário %s não é múltiplo de 3 e o resto da sua divisão por 3 é: %d", num, x);
-	}
+	if(x == 0) printf("O numero binario %s eh multiplo de 3", num);
+	else printf("O numero binario %s nao eh multiplo de 3 e o resto da sua divisao por 3 eh: %d", num, x);
 	return 0;
 }
